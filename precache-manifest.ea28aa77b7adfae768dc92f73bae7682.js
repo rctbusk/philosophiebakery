04920456410e3377aed50b7beac39a9d9027a5a6
rctbusk <ryan.busk@amplitude.com>
2020-09-28 12:05:45 -0700

    Updates

---
+++ precache-manifest.ea28aa77b7adfae768dc92f73bae7682.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73eb91de49b7fdeb967bd51c1296870",
+    "url": "/philosophiebakery/index.html"
+  },
+  {
+    "revision": "ca4e2b3b8f78fb3968c6",
+    "url": "/philosophiebakery/static/css/main.68f0a81a.chunk.css"
+  },
+  {
+    "revision": "9fb9ea58bf6730d6f41d",
+    "url": "/philosophiebakery/static/js/2.b2ba9095.chunk.js"
+  },
+  {
+    "revision": "fff33cd2b50e190fa354108153a8c2ae",
+    "url": "/philosophiebakery/static/js/2.b2ba9095.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca4e2b3b8f78fb3968c6",
+    "url": "/philosophiebakery/static/js/main.d3e2a168.chunk.js"
+  },
+  {
+    "revision": "56c1221908438f7ae9b5",
+    "url": "/philosophiebakery/static/js/runtime-main.5d154c07.js"
+  },
+  {
+    "revision": "eb0d8608899917f5275c0fce9809772f",
+    "url": "/philosophiebakery/static/media/NeutraText-Bold.eb0d8608.otf"
+  },
+  {
+    "revision": "cc211c16e02e4edede1df6e031fba0c0",
+    "url": "/philosophiebakery/static/media/NeutraText-Book.cc211c16.ttf"
+  },
+  {
+    "revision": "cf9277dcadd1c65874736d760ac8415a",
+    "url": "/philosophiebakery/static/media/NeutraText-Book.cf9277dc.otf"
+  },
+  {
+    "revision": "cfd27dddc37c12743d161aba06a44510",
+    "url": "/philosophiebakery/static/media/german_chocolate_pecan_cake.cfd27ddd.jpg"
+  }
+]);
