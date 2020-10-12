26cf5dd38557edd13aa35e9bf3379c485c41068f
rctbusk <ryan.busk@amplitude.com>
2020-10-12 14:46:29 -0700

    remove base name

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.render(
-  <BrowserRouter basename="/philosophiebakery">
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
