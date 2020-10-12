ca2f7ed73ad03e19f7a50cda518b2b2379b3e166
rctbusk <ryan.busk@amplitude.com>
2020-10-12 14:52:22 -0700

    Updates

---
+++ precache-manifest.05f156366fa307447deb2140353cdfa2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc9c1f99b11e3a58609523480f2c44e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db2f3f5c1056ffcdef27",
+    "url": "/static/css/main.4441dbea.chunk.css"
+  },
+  {
+    "revision": "5e6af0681fd4fd305610",
+    "url": "/static/js/2.4c50a610.chunk.js"
+  },
+  {
+    "revision": "fff33cd2b50e190fa354108153a8c2ae",
+    "url": "/static/js/2.4c50a610.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db2f3f5c1056ffcdef27",
+    "url": "/static/js/main.13866844.chunk.js"
+  },
+  {
+    "revision": "a5ac8ec9bb814639f7ae",
+    "url": "/static/js/runtime-main.f83139bb.js"
+  },
+  {
+    "revision": "eb0d8608899917f5275c0fce9809772f",
+    "url": "/static/media/NeutraText-Bold.eb0d8608.otf"
+  },
+  {
+    "revision": "cf9277dcadd1c65874736d760ac8415a",
+    "url": "/static/media/NeutraText-Book.cf9277dc.otf"
+  },
+  {
+    "revision": "2e876aab8822b5e69c018df744df88d4",
+    "url": "/static/media/easter_cookies.2e876aab.jpg"
+  },
+  {
+    "revision": "cfd27dddc37c12743d161aba06a44510",
+    "url": "/static/media/german_chocolate_pecan_cake.cfd27ddd.jpg"
+  },
+  {
+    "revision": "bae814806d4c6f7694c1e7b4e57f8c20",
+    "url": "/static/media/light_blue_to_white_weeding_cake.bae81480.jpg"
+  },
+  {
+    "revision": "d5d686146a0c049501f46346b459950c",
+    "url": "/static/media/patriotic_oreo_cake.d5d68614.jpg"
+  },
+  {
+    "revision": "a900e4f7e9817e85ef6cf276253550c5",
+    "url": "/static/media/pride_cookies.a900e4f7.jpg"
+  },
+  {
+    "revision": "d852ee29131fa7048fcd730ca4a01671",
+    "url": "/static/media/pumpkin_pie_placeholders.d852ee29.jpg"
+  },
+  {
+    "revision": "eba374a7af8a1478daad96fad1e37041",
+    "url": "/static/media/secondary_logo.eba374a7.png"
+  },
+  {
+    "revision": "189197ab948694b7d7fc22a6e1089e78",
+    "url": "/static/media/strawberry_cake.189197ab.jpg"
+  },
+  {
+    "revision": "8eee9d806fd07bbe090f3e0eaa513dd9",
+    "url": "/static/media/text_logo.8eee9d80.png"
+  },
+  {
+    "revision": "1c3cac6cc45460b6149b16301506a1ba",
+    "url": "/static/media/wedding_cake_flavor_cupcakes.1c3cac6c.jpg"
+  },
+  {
+    "revision": "1fa5408625231cdefc0d544e386fea22",
+    "url": "/static/media/yellow_cake.1fa54086.jpg"
+  },
+  {
+    "revision": "f3a38f31e4078cb8c48885b1285585db",
+    "url": "/static/media/yellow_cake_large.f3a38f31.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/philosophiebakery/precache-manifest.79946dd59219058e83f014f1900f2d25.js"
+  "/precache-manifest.05f156366fa307447deb2140353cdfa2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/philosophiebakery/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
