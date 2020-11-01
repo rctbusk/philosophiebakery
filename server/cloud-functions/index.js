2069506a4619a41d35d02c832896cb794a30224d
rctbusk <ryan.busk@amplitude.com>
2020-11-01 15:40:45 -0800

    add serverless functions for google

---
+++ server/cloud-functions/index.js
@@ -0,0 +1,36 @@
+/**
+ * Parses a 'multipart/form-data' upload request
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+
+// Node.js doesn't have a built-in multipart/form-data parsing library.
+// Instead, we can use the 'busboy' library from NPM to parse these requests.
+const Busboy = require("busboy");
+
+exports.uploadFile = (req, res) => {
+  if (req.method !== "POST") {
+    // Return a "method not allowed" error
+    return res.status(405).end();
+  }
+  const busboy = new Busboy({ headers: req.headers });
+
+  // This object will accumulate all the fields, keyed by their name
+  const fields = {};
+
+  // This object will accumulate all the uploaded files, keyed by their name.
+
+  // This code will process each non-file field in the form.
+  busboy.on("field", (fieldname, val) => {
+    // TODO(developer): Process submitted field values here
+    console.log(`Processed field ${fieldname}: ${val}.`);
+    fields[fieldname] = val;
+  });
+
+  // Triggered once all uploaded files are processed by Busboy.
+  // We still need to wait for the disk writes (saves) to complete.
+  busboy.on("finish", async () => {
+    res.send(`${JSON.stringify(fields)}`);
+  });
+};
